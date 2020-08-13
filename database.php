46d321dac83b0cf35d624021204a7b3277dbe77e
Israt Jahan Jui <jahanjuiisrat@gmail.com>
2020-08-14 00:52:01 +0600

    change

--- database.php
+++ database.php
@@ -3,6 +3,6 @@
 $con = mysqli_connect("localhost","root","","chatbox");
 
 //Test Connection
-if(mysqli_connect_errno()){
+if(mysqli_connect_error()){
 	echo 'Failed to connect to MySQL: '.mysqli_connect_error();
 }
